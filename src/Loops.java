d2ab7e5880dbd1be1830acba5de6c5968b76c03f
Manish Kumar <manish632004@gmail.com>
2024-09-10 11:39:39 +0530

    java conditionals

---
+++ src/Loops.java
@@ -0,0 +1,52 @@
+import java.util.Scanner ;
+public class Loops {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 5
+        //For Loop
+//        for (int num = 1;num <=5;num+=2){
+//            System.out.println(num);  //body
+//        }
+
+        // print number for 1 to n
+//        System.out.println("Please enter a limit : ");
+//        Scanner input = new Scanner(System.in);
+//        int n = input.nextInt() ;
+//
+//
+//        for (int num = 1; num <=n ; num++) {
+//            System.out.print(num + " ");
+//
+//        }
+
+//        while Loop syntax
+        /*
+        while(condition){
+//            body
+        }
+        */
+        // printing numbers from 1 to 5 using while loop
+//
+//        int m = 1;
+//
+//        while(m<=5){
+//            System.out.println(m);
+//            m++;
+//        }
+
+        // Do while loop
+
+        /*
+        do{
+
+        //body
+
+        } while(condition);
+         */
+        int l = 1;
+        do{
+            System.out.println(l);
+            l+=1;
+
+        } while (l<=5);
+    }
+}
